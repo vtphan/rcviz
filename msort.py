aa79d0e6643af1e56ff64ad84bd4bb4184f4e396
vtphan <vphan@memphis.edu>
2014-05-11 16:33:36 -0500

    .

--- msort.py
+++ msort.py
@@ -3,23 +3,12 @@ import copy
 
 def merge(y, z):
     result = []
-    while (len(y) > 0) or (len(z) > 0):
-        if len(y) > 0 and len(z) > 0:
-            if y[0] > z[0]:
-                result.append(z[0])
-                z.pop(0)
-            else:
-                result.append(y[0])
-                y.pop(0)
-        elif len(z) > 0:
-            for i in z:
-                result.append(i)
-                z.pop(0)
+    while (len(y) > 0) and (len(z) > 0):
+        if y[0] > z[0]:
+            result.append(z.pop(0))
         else:
-            for i in y:
-                result.append(i)
-                y.pop(0)
-    return result
+            result.append(y.pop(0))
+    return result + y + z
 
 @viz
 def msort(x):
@@ -30,5 +19,5 @@ def msort(x):
     z = msort(x[mid:])
     return merge(y,z)
 
-msort([4,8,3,5,1,2,7,6])
+msort([4,8,3,10,5,1,2,15,7,6])
 callgraph.render("ms.png")
