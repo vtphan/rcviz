361db0eeaa0fe60d5cf3879b83ba888e140d48a4
vtphan <vphan@memphis.edu>
2016-01-27 13:25:08 -0600

    simple examples

---
+++ examples.py
@@ -0,0 +1,27 @@
+from rcviz import callgraph, viz
+
+Table = {}
+
+@viz
+def fib2(n):
+	if n not in Table:
+		if n<2:
+			Table[n] = n
+		else:
+			Table[n] = fib2(n-1) + fib2(n-2)
+	return Table[n] 
+
+@viz
+def fib(n):
+	if n<2:
+		return n
+	else:
+		return fib(n-1) + fib(n-2)
+
+@viz
+def fact(n):
+	if n==0: return 1
+	return n * fact(n-1)
+
+fib(11)
+callgraph.render("out.svg")
